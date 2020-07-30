fac38fb809781c8419bfb5da7b67de2c971a959a
Daniel Gould <dmgould310@gmail.com>
2020-07-30 12:20:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,19 +1,12 @@
-def reverse_each_word(array)
-  new_array = array.split(" ")
-  reversed_array = new_array.each {|x| x.reverse!}
-  return reversed_array.join(" ")
-end
+def reverse_each_word(string)
+  newarr = string.split(" ")
+  final_array = []
+  reversed_array = newarr.each do |word|
+  final_array.push(word.reverse)
+end 
 
 
-def reverse_each_word(array)
-  new_array = array.split(" ")
-  new_array.collect {|x| x.reverse!}
-  new_array.join(" ")
-end
 
 
-def reverse_each_word(array)
-  array.split(" ").collect {|x| x.reverse!}.join(" ")
-end
 
 
