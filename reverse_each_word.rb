890810195325142e710e11bb7a2593b1b956e2c5
Daniel Gould <dmgould310@gmail.com>
2020-07-30 12:28:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,12 +7,7 @@ end
   return final_array.join(" ")
 end 
 
-def reverse_each_word(string)
-  newarr = string.split(" ")
-  reversed_array = newarr.map do |word|
-  word.reverse 
-  end
-end
+
     
 
 
